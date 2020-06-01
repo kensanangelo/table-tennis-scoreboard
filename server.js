df1e9dd7f8b9c58559c8d4921b801c400ad0ce33
Ken San Angelo <ken@kensanangelo.com>
2020-05-31 21:29:01 -0400

    Sets up mongodb and connection to remote db (Mongo Atlas)

--- server.js
+++ server.js
@@ -2,13 +2,36 @@ const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 var path = require("path");
-
 const app = express();
 const port = process.env.PORT || 5000;
+
+//Sets build folder public so client can access js and css files
 app.use('/client/build', express.static(__dirname + '/client/build'))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connects to DB
+//* All of the API calls that need the DB go in here
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect(config.connectionString, {
+   useUnifiedTopology: true
+ }, (err, client) => {
+   if (err) return console.error(err)
+   console.log('Connected to Database')
+
+   const db = client.db('scoreboard')
+
+   app.post('/api/submit-game', (req, res) => {
+      //Checks if client sent correct token
+      if(req.body.token === config.serverToken){
+         sendResponse(res, {message: `I received your POST request.`});
+      }else{
+         res.status(501)
+            .send({message: 'Bad token. Permission denied'});
+      }
+   });
+ })
 
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/client/build/index.html')
@@ -17,17 +40,7 @@ app.get('/', function(req, res) {
 app.get('/api/hello', (req, res) => {
    // For now, returns a success message
    // TODO return useful info from DB
-   sendResponse(res, { express: 'Test Request' });
-});
-
-app.post('/api/submit-game', (req, res) => {
-   //Checks if client sent correct token
-   if(req.body.token === config.serverToken){
-      sendResponse(res, {message: `I received your POST request.`});
-   }else{
-      res.status(501)
-         .send({message: 'Bad token. Permission denied'});
-   }
+   sendResponse(res, { express: 'Request Successful. The API is running' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
