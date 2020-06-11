2b4dbb0b94b788c300e5ecb607796484f3625aad
Ken <ken@big.vision>
2020-06-11 10:10:06 -0400

    Adds build folder so it can be pulled on the server / other computers

---
+++ client/build/precache-manifest.b8a9a18c2aaa514cd41a1b20669bffa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b496d034f5aa55258cc0c39878ea9d4",
+    "url": "/client/build/index.html"
+  },
+  {
+    "revision": "4522508f70986c8f0cdf",
+    "url": "/client/build/static/css/main.f7b0b1b3.chunk.css"
+  },
+  {
+    "revision": "863bb86bf5538f903765",
+    "url": "/client/build/static/js/2.b9a9a90f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/client/build/static/js/2.b9a9a90f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4522508f70986c8f0cdf",
+    "url": "/client/build/static/js/main.8b7b6430.chunk.js"
+  },
+  {
+    "revision": "451d115f33157e51d982",
+    "url": "/client/build/static/js/runtime-main.0043efaa.js"
+  }
+]);
