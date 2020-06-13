94523f51c1e73e60233871eca39729f9a339ad14
Ken <ken@big.vision>
2020-06-13 18:45:49 -0400

    Adds names to Game over screen scores so you know who played

--- client/src/components/GameOver/GameOver.js
+++ client/src/components/GameOver/GameOver.js
@@ -12,7 +12,7 @@ export default class GameOver extends React.Component {
 				<h2 className="gameover__gameover">Game Over</h2>
 				<h1 className="gameover__winner">{this.props.winner} wins!</h1>
 
-				<p className="gameover__score">{this.props.homeScore}-{this.props.awayScore}</p>
+				<p className="gameover__score">{this.props.home.name} {this.props.home.score} - {this.props.away.score} {this.props.away.name}</p>
 				<button 
 					className="gameover__btn-new btn-main"
 					onClick={() => this.props.setNewGame()}>
