4102160dc7a7bd6bd6183aaa277ff67d421a3721
Ken San Angelo <ken@kensanangelo.com>
2020-08-21 22:00:41 -0400

    Moves api router to it's own file

---
+++ routes/apiRouter.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+const config = require('../config.json');
+
+const {
+	checkToken,
+	sendError,
+	sendResponse,
+	submitGametoDB,
+	getPlayers,
+	getStats,
+} = require('../utils/utils');
+
+// Connects to DB
+//* All of the API calls that need the DB go in here
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(
+	config.connectionString,
+	{
+		useUnifiedTopology: true,
+	},
+	(err, client) => {
+		if (err) return console.error(err);
+		console.log('\x1b[44m\x1b[37m', 'Connected to Database', '\x1b[0m\n');
+
+		const db = client.db('scoreboard');
+		const playersCollection = db.collection('players');
+		const gamesCollection = db.collection('games');
+
+		router.post('/submit-game', checkToken, (req, res) => {
+			submitGametoDB(gamesCollection, req.body).then((response) => {
+				if (response === '200') {
+					console.log('\x1b[32m', 'Game saved correctly');
+
+					sendResponse(res, {
+						status: 200,
+						message: `Game saved correctly.`,
+					});
+				} else {
+					console.log('\x1b[31m', `DB ERROR: ${response.errmsg}`);
+
+					sendError(res, 500, {
+						message: 'DB Insertion Failed. Reason: ' + response.errmsg,
+					});
+				}
+			});
+		});
+
+		router.post('/get-stats', checkToken, (req, res) => {
+			getStats(gamesCollection, playersCollection).then((games, players) => {
+				console.log('\x1b[32m', 'Req: Retrieved all games');
+				sendResponse(res, { status: 200, games, players });
+			});
+		});
+
+		router.post('/get-players', checkToken, (req, res) => {
+			getPlayers(playersCollection).then((players) => {
+				console.log('\x1b[32m', 'Req: Retrieved all players');
+				sendResponse(res, { status: 200, players });
+			});
+		});
+	}
+);
+
+router.get('/hello', (req, res) => {
+	//* This is used to hit up the API to make sure it's responding correctly
+	sendResponse(res, { msg: 'Connected to API' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const generateTerminalEffect = require('./utils/terminal-effect-generator');
 
-const {
-	checkToken,
-	sendError,
-	sendResponse,
-	submitGametoDB,
-	getPlayers,
-	getStats,
-} = require('./utils/utils');
+const config = require('./config');
+const apiRouter = require('./routes/apiRouter');
+
+const { checkToken } = require('./utils/utils');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,61 +18,7 @@ app.use(
 	express.static(__dirname + '/client/build/static')
 );
 
-// Connects to DB
-//* All of the API calls that need the DB go in here
-const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(
-	config.connectionString,
-	{
-		useUnifiedTopology: true,
-	},
-	(err, client) => {
-		if (err) return console.error(err);
-		console.log('\x1b[44m\x1b[37m', 'Connected to Database', '\x1b[0m\n');
-
-		const db = client.db('scoreboard');
-		const playersCollection = db.collection('players');
-		const gamesCollection = db.collection('games');
-
-		app.post('/api/submit-game', checkToken, (req, res) => {
-			submitGametoDB(gamesCollection, req.body).then((response) => {
-				if (response === '200') {
-					console.log('\x1b[32m', 'Game saved correctly');
-
-					sendResponse(res, {
-						status: 200,
-						message: `Game saved correctly.`,
-					});
-				} else {
-					console.log('\x1b[31m', `DB ERROR: ${response.errmsg}`);
-
-					sendError(res, 500, {
-						message: 'DB Insertion Failed. Reason: ' + response.errmsg,
-					});
-				}
-			});
-		});
-
-		app.post('/api/get-stats', checkToken, (req, res) => {
-			getStats(gamesCollection, playersCollection).then((games, players) => {
-				console.log('\x1b[32m', 'Req: Retrieved all games');
-				sendResponse(res, { status: 200, games, players });
-			});
-		});
-
-		app.post('/api/get-players', checkToken, (req, res) => {
-			getPlayers(playersCollection).then((players) => {
-				console.log('\x1b[32m', 'Req: Retrieved all players');
-				sendResponse(res, { status: 200, players });
-			});
-		});
-	}
-);
-
-app.get('/api/hello', (req, res) => {
-	//* This is used to hit up the API to make sure it's responding correctly
-	sendResponse(res, { msg: 'Connected to API' });
-});
+app.use('/api', apiRouter);
 
 //! Maybe delete this?
 //! Depends on if client can access server to get client files
